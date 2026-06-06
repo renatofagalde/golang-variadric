4b7a0990000d9bf86baecd0472974f4628dc9c69
Renato Fagalde <renato@likwi.com.br>
2026-06-06 17:31:46 -0300

    Add FileType field to Livro entity

--- internal/livro/domain/entity.go
+++ internal/livro/domain/entity.go
@@ -1,12 +1,15 @@
 package domain
 
-import "time"
+import (
+	"time"
+)
 
 type Livro struct {
 	ID        int64
 	Titulo    string
 	Autor     string
 	IsDigital bool
+	FileType  FileType
 	IsActive  bool
 	CreatedAt time.Time
 }
