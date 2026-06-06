8ba5b18fba6a6d3f0d08253e4aa96b38f6133f3d
Renato Fagalde <renato@likwi.com.br>
2026-06-06 17:32:00 -0300

    Add MarcarTipo option to configure FileType in CadastroOptions

--- internal/livro/application/options.go
+++ internal/livro/application/options.go
@@ -1,9 +1,12 @@
 package application
 
+import "golang-variadric/internal/livro/domain"
+
 // CadastroOpts agrega as flags de configuração do cadastro.
 // É o "saco de defaults": zero value = livro físico.
 type CadastroOptions struct {
 	IsDigital bool
+	domain.FileType
 }
 
 // Option configura uma operação de cadastro.
@@ -14,6 +17,13 @@ type Option func(options *CadastroOptions)
 func MarcarLivroSendoDigital() Option {
 	return func(o *CadastroOptions) {
 		o.IsDigital = true
+	}
+}
+
+func MarcarTipo(fileTye domain.FileType) Option {
+	return func(o *CadastroOptions) {
+		o.IsDigital = true
+		o.FileType = fileTye
 	}
 }
 
