0cf78ca37a5a8f6e3701f12955272927a4326f2c
Renato Fagalde <renato@likwi.com.br>
2026-06-06 13:46:37 -0300

    Add Create method to Livro repository

---
+++ interal/livro/infrastructure/repository/livro_create_repository.go
@@ -0,0 +1,22 @@
+package repository
+
+import (
+	"context"
+	"golang-variadric/interal/livro/domain"
+	"time"
+)
+
+func (dao *livroDAO) Create(ctx context.Context, livro *domain.Livro) error {
+	if livro == nil {
+		return ErrInvalidInput
+	}
+
+	dao.mu.Lock()
+	defer dao.mu.Unlock()
+	livro.ID = dao.nextID
+	livro.CreatedAt = time.Now().UTC()
+	dao.nextID++
+
+	dao.data[livro.ID] = *livro
+	return nil
+}
