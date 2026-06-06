4906879beeaec6fa4b431ca1a94ce0668d27b1b7
Renato Fagalde <renato@likwi.com.br>
2026-06-06 13:46:43 -0300

    Add GetByID method to Livro repository

---
+++ interal/livro/infrastructure/repository/livro_get_by_id_repository.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"context"
+	"golang-variadric/interal/livro/domain"
+)
+
+func (dao *livroDAO) GetByID(ctx context.Context, id int64) (*domain.Livro, error) {
+	dao.mu.Lock()
+	defer dao.mu.Unlock()
+	livro, ok := dao.data[id]
+	if !ok {
+		return nil, ErrNotFound
+	}
+	return &livro, nil
+}
