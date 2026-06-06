a2cb66db65acbb4252bb5a4b1118773bc9b9f936
Renato Fagalde <renato@likwi.com.br>
2026-06-06 13:47:00 -0300

    Add CLI entry point for Livro use case

---
+++ cmd/cli/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang-variadric/interal/livro/application"
+	"golang-variadric/interal/livro/infrastructure/repository"
+	"log"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	livroRepository := repository.NewLivroRepository()
+	service := application.NewLivroService(livroRepository)
+
+	//codigo em producao:
+	livroHobbit, err := service.Cadastrar(ctx, "O Hobbit", "Tolkien")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("created: %+v\n", *livroHobbit)
+
+	livroCC, err := service.Cadastrar(ctx, "Clean Code", "Robert Martin")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("created: %+v\n", *livroCC)
+}
