192348e21d4d7d28b29a6ca6bd01f368c4781cbb
Renato Fagalde <renato@likwi.com.br>
2026-06-06 17:13:02 -0300

    Add support for variadic options in Cadastrar method of livroUseCase

--- internal/livro/application/cadastrar_livro.go
+++ internal/livro/application/cadastrar_livro.go
@@ -5,15 +5,18 @@ import (
 	"golang-variadric/internal/livro/domain"
 )
 
-func (u *livroUseCase) Cadastrar(ctx context.Context, titulo, author string) (*domain.Livro, error) {
+func (u *livroUseCase) Cadastrar(ctx context.Context, titulo, author string, opts ...Option) (*domain.Livro, error) {
 	if titulo == "" || author == "" {
 		return nil, ErrInvalidInput
 	}
 
+	options := ResolveOptions(opts...)
+
 	livro := &domain.Livro{
-		Titulo:   titulo,
-		Autor:    author,
-		IsActive: true,
+		Titulo:    titulo,
+		Autor:     author,
+		IsDigital: options.IsDigital,
+		IsActive:  true,
 	}
 	if err := u.repository.Create(ctx, livro); err != nil {
 		return nil, err
